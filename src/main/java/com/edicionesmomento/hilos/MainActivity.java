d5f274528868563efaf767bcabecc39a4c2045d7
Fran Núñez <trexment@gmail.com>
2019-03-05 16:59:51 +0100

    aparece el progressbar y no se bloquea

--- src/main/java/com/edicionesmomento/hilos/MainActivity.java
+++ src/main/java/com/edicionesmomento/hilos/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView mImageView;
     Button btn;
+    ProgressBar progreso;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
         mImageView = findViewById(R.id.imageView);
         btn = findViewById(R.id.button);
+        progreso = findViewById(R.id.progressBar);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
-                    Thread.sleep(10000); //Hilo que lo manda a dormir 10 seg.
-                    Toast.makeText(MainActivity.this, "Proceso terminado", Toast.LENGTH_SHORT).show();
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
+               progreso.setVisibility(View.VISIBLE);
+               new Hilo1().start();
             }
         });
+    }
+
+    class Hilo1 extends Thread{
+        @Override
+        public void run(){
+            try{
+                Thread.sleep(10000); //Hilo que lo manda a dormir 10 seg.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Proceso Terminado", Toast.LENGTH_SHORT).show();
+                        progreso.setVisibility(View.INVISIBLE);
+                    }
+                });
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
